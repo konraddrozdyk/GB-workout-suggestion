65f60a2befa58aad7f4443f647ba92a78585b814
konraddrozdyk <konrad.drozdyk@gmail.com>
2024-12-29 13:11:20 +0100

    Configuration improvements and style modifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+// A new router instance
+const router = createRouter({ routeTree });
+// Render the app
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
